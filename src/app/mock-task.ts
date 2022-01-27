79824c6061ed8c5133ad5ebf8fa130e00f2b4e9e
Juchambo <juchambo@gmail.com>
2022-01-27 13:10:25 -0300

    agregamos Mock de tareas e interface de las mismas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,26 @@
+export const TASK = [
+    {
+        id: 1,
+        text: "terminar nuestro primer módulo de Angular",
+        day: "Enero 28 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "hace compras",
+        day: "Enero 28 a las 18:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar Bootstrap",
+        day: "Enero 29 a las 12:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer Histora",
+        day: "Enero 30 a las 12:00",
+        reminder: false
+    },
+]
